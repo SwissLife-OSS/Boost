0096e843b318543c78bed03b95e862cdb558596c
Philippe Birbaum <tree@gmx.ch>
2021-05-04 07:33:50 +0200

    Weblinks in local repo

---
+++ src/Tool/src/Boost.Abstractions/Git/IGitRemoteService.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Boost.Settings;
+
+namespace Boost.Git
+{
+    public interface IGitRemoteService
+    {
+        Task<GitRemoteRepository> GetAsync(
+            Guid serviceId,
+            string id,
+            CancellationToken cancellationToken);
+        Task<GitRemoteRepository?> GetByRemoteReferenceAsync(Guid serviceId, IGitRemoteReference reference, string name, CancellationToken cancellationToken);
+        Task<byte[]?> GetFileContentAsync(
+            Guid serviceId,
+            string id,
+            string path,
+            CancellationToken cancellationToken);
+
+        Task<IEnumerable<GitRemoteCommit>> GetLastCommitsAsync(
+            Guid serviceId,
+            string id,
+            int top,
+            CancellationToken cancellationToken);
+    }
+}
--- src/Tool/src/Boost.Tool/VersionCommand.cs
+++ src/Tool/src/Boost.Tool/VersionCommand.cs
@@ -30,6 +30,7 @@ namespace Boost.Commands
             IConsole console)
         {
             var currentVersion = Program.GetVersion();
+
             Console.WriteLine($"Installed:\t\t{currentVersion}");
 
             NugetPackageInfo? version = await _nugetService
